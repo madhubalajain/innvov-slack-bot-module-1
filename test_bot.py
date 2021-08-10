a8aec7486bd07f9d95f19af42d60876064b2927c
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-08-10 12:26:55 +0530

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -7,9 +7,16 @@ def test_assert_tuple():
 
 
 @pytest.mark.REGRESSION
-def test_assert_contain():
+def test_assert_contain1():
     assert 1 in (1, 2, 3)
 
+@pytest.mark.REGRESSION
+def test_assert_contain2():
+    assert 2 in (1, 2, 3)
+    
+@pytest.mark.REGRESSION
+def test_assert_contain3():
+    assert 3 in (1, 2, 3)
 
 @pytest.mark.CONTRACT
 def test_assert_addition():
