49606c309f82da460ea24790282c00ec8117065f
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-08-10 12:27:27 +0530

    Update test.py

--- service-functionaltest/test.py
+++ service-functionaltest/test.py
@@ -9,8 +9,20 @@ def test_assert_tuple():
 @pytest.mark.REGRESSION
 def test_assert_contain():
     assert 1 in (1, 2, 3)
+    
+@pytest.mark.REGRESSION
+def test_assert_contain1():
+    assert 1 in (1, 2, 3)
+
+@pytest.mark.REGRESSION
+def test_assert_contain2():
+    assert 2 in (1, 2, 3)
+    
+@pytest.mark.REGRESSION
+def test_assert_contain3():
+    assert 3 in (1, 2, 3)
 
 
-@pytest.mark.CONTARCT
+@pytest.mark.CONTRACT
 def test_assert_addition():
     assert 1 + 2 == 3
